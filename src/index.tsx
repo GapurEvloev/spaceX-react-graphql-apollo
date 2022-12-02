7f37b0d6a5a1cc6fd185f4cf2854c71184128848
gapur <kaloycant@gmail.com>
2022-12-02 16:33:06 +0100

    fix last

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,13 @@ import client from "./apollo/client";
 import {theme} from "./theme";
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
-import { REPO_NAME } from './constants/repo';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${REPO_NAME}/`}>
+    <BrowserRouter>
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <App />
