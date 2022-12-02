6e66fd408cba7fedaeb0b4a738409ce14d7d0e48
gapur <kaloycant@gmail.com>
2022-12-02 17:03:44 +0100

    remove import type

--- src/components/ShipCard/ShipCard.generated.ts
+++ src/components/ShipCard/ShipCard.generated.ts
@@ -1,5 +1,3 @@
-import * as Types from '../../types.generated';
-
 import { gql } from '@apollo/client';
 export type ShipCardFragment = { __typename?: 'Ship', name?: string | null, image?: string | null, type?: string | null, url?: string | null, id?: string | null };
 
